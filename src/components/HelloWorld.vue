f47718a8d43454db1fcc2191e76bca8f49fd3716
weiweihu <1639150415@qq.com>
2020-12-02 00:10:27 +0800

    第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>上面是父组件传递过来的属性</p>
+    <h1>测试页面 哈啊哈哈哈</h1>
+    <h2>绩效考核</h2>
+    <h2>测试项目</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>这是about测试的组件</h1>
+    <h1>最新的vue3尝鲜版本的测试</h1>
+    <h1>ant-design-vue 2.0.0测试版本的使用</h1>
+    <h1>最新搭建的脚手架项目</h1>
+  </div>
+</template>
